88fa44ee4f212be81e1c75aaec0c7763ecf52a8e
yatennn <prostoboris_1@mail.ru>
2021-05-30 20:57:04 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/бингус.jpg_large') {
+      myImage.setAttribute ('src','images/bengus.jpg');
+    } else {
+      myImage.setAttribute ('src','images/бингус.jpg_large');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+}
+myButton.onclick = function() {
+  setUserName();
+}
